37eef46ffd61f56ad5c490d00102cef7cb009347
chsurajkumar4 <39794980+chsurajkumar4@users.noreply.github.com>
2018-10-01 21:54:17 +0530

    Add files via upload

---
+++ transaction review app/transaction review app/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace transaction_review_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.review = new System.Windows.Forms.Button();
+            this.refer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
+            this.namebox = new System.Windows.Forms.TextBox();
+            this.review_label = new System.Windows.Forms.Label();
+            this.reviewbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.transaction = new System.Windows.Forms.Label();
+            this.block_no = new System.Windows.Forms.Label();
+            this.blocknobox = new System.Windows.Forms.TextBox();
+            this.trans_id = new System.Windows.Forms.Label();
+            this.txidbox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Send_to = new System.Windows.Forms.Label();
+            this.Send_to_box = new System.Windows.Forms.TextBox();
+            this.labelmsg = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flo_label = new System.Windows.Forms.Label();
+            this.flo_box = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // review
+            // 
+            this.review.BackColor = System.Drawing.Color.Purple;
+            this.review.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.review.Location = new System.Drawing.Point(52, 112);
+            this.review.Name = "review";
+            this.review.Size = new System.Drawing.Size(343, 33);
+            this.review.TabIndex = 0;
+            this.review.Text = "Click to Submit a Review";
+            this.review.UseVisualStyleBackColor = false;
+            this.review.Click += new System.EventHandler(this.review_Click);
+            // 
+            // refer
+            // 
+            this.refer.BackColor = System.Drawing.Color.Purple;
+            this.refer.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.refer.Location = new System.Drawing.Point(586, 112);
+            this.refer.Name = "refer";
+            this.refer.Size = new System.Drawing.Size(315, 33);
+            this.refer.TabIndex = 1;
+            this.refer.Text = "Click to Refer Reviews";
+            this.refer.UseVisualStyleBackColor = false;
+            this.refer.Click += new System.EventHandler(this.refer_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(352, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(283, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Flo Review Systems";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.BackColor = System.Drawing.Color.DarkOrange;
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(52, 202);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(63, 20);
+            this.name.TabIndex = 4;
+            this.name.Text = "Name:";
+            this.name.Visible = false;
+            // 
+            // namebox
+            // 
+            this.namebox.Location = new System.Drawing.Point(149, 202);
+            this.namebox.Name = "namebox";
+            this.namebox.Size = new System.Drawing.Size(246, 22);
+            this.namebox.TabIndex = 5;
+            this.namebox.Visible = false;
+            this.namebox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // review_label
+            // 
+            this.review_label.AutoSize = true;
+            this.review_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.review_label.Location = new System.Drawing.Point(48, 339);
+            this.review_label.Name = "review_label";
+            this.review_label.Size = new System.Drawing.Size(75, 20);
+            this.review_label.TabIndex = 6;
+            this.review_label.Text = "Review:";
+            this.review_label.Visible = false;
+            // 
+            // reviewbox
+            // 
+            this.reviewbox.Location = new System.Drawing.Point(149, 343);
+            this.reviewbox.Multiline = true;
+            this.reviewbox.Name = "reviewbox";
+            this.reviewbox.Size = new System.Drawing.Size(246, 55);
+            this.reviewbox.TabIndex = 7;
+            this.reviewbox.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "SUBMIT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // transaction
+            // 
+            this.transaction.AutoSize = true;
+            this.transaction.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.transaction.Location = new System.Drawing.Point(603, 157);
+            this.transaction.Name = "transaction";
+            this.transaction.Size = new System.Drawing.Size(270, 22);
+            this.transaction.TabIndex = 9;
+            this.transaction.Text = "Enter TRANSACTION details";
+            this.transaction.Visible = false;
+            // 
+            // block_no
+            // 
+            this.block_no.AutoSize = true;
+            this.block_no.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.block_no.Location = new System.Drawing.Point(583, 207);
+            this.block_no.Name = "block_no";
+            this.block_no.Size = new System.Drawing.Size(136, 40);
+            this.block_no.TabIndex = 10;
+            this.block_no.Text = "Block no to be \r\nsearched from";
+            this.block_no.Visible = false;
+            this.block_no.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // blocknobox
+            // 
+            this.blocknobox.Location = new System.Drawing.Point(763, 207);
+            this.blocknobox.Name = "blocknobox";
+            this.blocknobox.Size = new System.Drawing.Size(176, 22);
+            this.blocknobox.TabIndex = 11;
+            this.blocknobox.Visible = false;
+            // 
+            // trans_id
+            // 
+            this.trans_id.AutoSize = true;
+            this.trans_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trans_id.Location = new System.Drawing.Point(583, 270);
+            this.trans_id.Name = "trans_id";
+            this.trans_id.Size = new System.Drawing.Size(133, 20);
+            this.trans_id.TabIndex = 12;
+            this.trans_id.Text = "Transaction ID";
+            this.trans_id.Visible = false;
+            // 
+            // txidbox2
+            // 
+            this.txidbox2.Location = new System.Drawing.Point(763, 270);
+            this.txidbox2.Name = "txidbox2";
+            this.txidbox2.Size = new System.Drawing.Size(176, 22);
+            this.txidbox2.TabIndex = 15;
+            this.txidbox2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(693, 339);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 30);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "SUBMIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Send_to
+            // 
+            this.Send_to.AutoSize = true;
+            this.Send_to.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Send_to.Location = new System.Drawing.Point(52, 253);
+            this.Send_to.Name = "Send_to";
+            this.Send_to.Size = new System.Drawing.Size(84, 20);
+            this.Send_to.TabIndex = 17;
+            this.Send_to.Text = "Send To:";
+            this.Send_to.Visible = false;
+            // 
+            // Send_to_box
+            // 
+            this.Send_to_box.Location = new System.Drawing.Point(149, 253);
+            this.Send_to_box.Name = "Send_to_box";
+            this.Send_to_box.Size = new System.Drawing.Size(246, 22);
+            this.Send_to_box.TabIndex = 18;
+            this.Send_to_box.Visible = false;
+            // 
+            // labelmsg
+            // 
+            this.labelmsg.AutoSize = true;
+            this.labelmsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelmsg.Location = new System.Drawing.Point(221, 432);
+            this.labelmsg.Name = "labelmsg";
+            this.labelmsg.Size = new System.Drawing.Size(0, 20);
+            this.labelmsg.TabIndex = 19;
+            this.labelmsg.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(594, 397);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // flo_label
+            // 
+            this.flo_label.AutoSize = true;
+            this.flo_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flo_label.Location = new System.Drawing.Point(53, 300);
+            this.flo_label.Name = "flo_label";
+            this.flo_label.Size = new System.Drawing.Size(41, 20);
+            this.flo_label.TabIndex = 21;
+            this.flo_label.Text = "Flo:";
+            this.flo_label.Visible = false;
+            // 
+            // flo_box
+            // 
+            this.flo_box.Location = new System.Drawing.Point(149, 298);
+            this.flo_box.Name = "flo_box";
+            this.flo_box.Size = new System.Drawing.Size(246, 22);
+            this.flo_box.TabIndex = 22;
+            this.flo_box.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(963, 493);
+            this.Controls.Add(this.flo_box);
+            this.Controls.Add(this.flo_label);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelmsg);
+            this.Controls.Add(this.Send_to_box);
+            this.Controls.Add(this.Send_to);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txidbox2);
+            this.Controls.Add(this.trans_id);
+            this.Controls.Add(this.blocknobox);
+            this.Controls.Add(this.block_no);
+            this.Controls.Add(this.transaction);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.reviewbox);
+            this.Controls.Add(this.review_label);
+            this.Controls.Add(this.namebox);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.refer);
+            this.Controls.Add(this.review);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Name = "Form1";
+            this.Text = "Flo review app";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button review;
+        private System.Windows.Forms.Button refer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.TextBox namebox;
+        private System.Windows.Forms.Label review_label;
+        private System.Windows.Forms.TextBox reviewbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label transaction;
+        private System.Windows.Forms.Label block_no;
+        private System.Windows.Forms.TextBox blocknobox;
+        private System.Windows.Forms.Label trans_id;
+        private System.Windows.Forms.TextBox txidbox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Send_to;
+        private System.Windows.Forms.TextBox Send_to_box;
+        private System.Windows.Forms.Label labelmsg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label flo_label;
+        private System.Windows.Forms.TextBox flo_box;
+    }
+}
+
---
+++ transaction review app/transaction review app/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FloSDK.Exceptions;
+using FloSDK.Methods;
+using Newtonsoft.Json.Linq;
+
+namespace transaction_review_app
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string username = ConfigurationManager.AppSettings.Get("rpcusername");
+            string password = ConfigurationManager.AppSettings.Get("rpcpassword");
+            string wallet_url = ConfigurationManager.AppSettings.Get("wallet_url");
+            string wallet_port = ConfigurationManager.AppSettings.Get("wallet_port");
+
+            RpcMethods rpc = new RpcMethods(username, password, wallet_url, wallet_port);
+            
+
+            try
+            {
+                
+                string Name = namebox.Text;
+                string Send_to = Send_to_box.Text;
+                string flo_coin = flo_box.Text;
+                string Review = reviewbox.Text;
+                int flocoin = 0;
+                flocoin = Convert.ToInt32(flo_coin);
+
+                string flodata = "Review from:" + Name + "-- " + Review;
+
+                JObject jobj = JObject.Parse(rpc.SendToAddress(Send_to, flocoin, "review purpose", "review purpose", false, false, 1, "UNSET", flodata));
+                if (string.IsNullOrEmpty(jobj["error"].ToString()))
+                {
+                    
+                    labelmsg.Text = "Review saved successfully to FLO";
+                    labelmsg.Visible = true;
+                    labelmsg.ForeColor = Color.Green;
+                }
+                else
+                {
+                    labelmsg.Text = "Error!!!" + "could not retrieve any transaction details..plzz check ur transaction id";
+                    labelmsg.Visible = true;
+                    labelmsg.ForeColor = Color.Red;
+                }
+
+            }
+            catch (RpcInternalServerErrorException ex)
+            {
+                var errCode = 0;
+                var errMessage = string.Empty;
+                if (ex.RpcErrorCode.GetHashCode() != 0)
+                {
+                    errCode = ex.RpcErrorCode.GetHashCode();
+                    errMessage = ex.RpcErrorCode.ToString();
+                }
+                Console.WriteLine("Exception :" + errCode + "-" + errMessage);
+                
+
+                labelmsg.Text = "Error saving details succefully to flo";
+                labelmsg.ForeColor = Color.Red;
+                labelmsg.Visible = true;
+                labelmsg.Visible = true;
+
+
+
+            }
+            catch (Exception ex1)
+            {
+                Console.WriteLine("Exception :" + ex1.ToString());
+                labelmsg.Text = "Error saving details succefully to system";
+                labelmsg.ForeColor = Color.Red;
+                labelmsg.Visible = true;
+                labelmsg.Visible = true;
+            }
+        }
+
+        private void sendtoaddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void review_Click(object sender, EventArgs e)
+        {
+            name.Visible = true;
+            Send_to.Visible = true;
+            review_label.Visible = true;
+            namebox.Visible = true;
+            Send_to_box.Visible = true;
+            reviewbox.Visible = true;
+            button1.Visible = true;
+            flo_label.Visible = true;
+            flo_box.Visible = true;
+        }
+
+        private void refer_Click(object sender, EventArgs e)
+        {
+            block_no.Visible = true;
+            trans_id.Visible = true;
+            blocknobox.Visible = true;
+            txidbox2.Visible = true;
+            transaction.Visible = true;
+            button2.Visible = true;
+            flo_label.Visible = true;
+            flo_box.Visible = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string username = ConfigurationManager.AppSettings.Get("rpcusername");
+            string password = ConfigurationManager.AppSettings.Get("rpcpassword");
+            string wallet_url = ConfigurationManager.AppSettings.Get("wallet_url");
+            string wallet_port = ConfigurationManager.AppSettings.Get("wallet_port");
+
+            RpcMethods rpc = new RpcMethods(username, password, wallet_url, wallet_port);
+            try
+            {
+                string block_no = blocknobox.Text;
+                string tx_id2 = txidbox2.Text;
+
+                JObject obj1 = JObject.Parse(rpc.GetRawTransaction(tx_id2));
+                if (string.IsNullOrEmpty(obj1["error"].ToString()))
+                {
+                    string flodata = "";
+                    flodata = obj1["result"]["flodata"].ToString();
+                    label2.Text = flodata;
+                    label2.ForeColor = Color.Green;
+                    label2.Visible = true;
+                    
+
+                }
+                else
+                {
+                    label2.Text = "Error retrieving reviews from the flo!!";
+                    label2.ForeColor = Color.Green;
+                    label2.Visible = true;
+                }
+
+
+
+            }
+            catch (RpcInternalServerErrorException ex)
+            {
+                var errCode = 0;
+                var errMessage = string.Empty;
+                if (ex.RpcErrorCode.GetHashCode() != 0)
+                {
+                    errCode = ex.RpcErrorCode.GetHashCode();
+                    errMessage = ex.RpcErrorCode.ToString();
+                }
+                Console.WriteLine("Exception :" + errCode + "-" + errMessage);
+
+
+                label2.Text = "Error saving details succefully to flo";
+                label2.ForeColor = Color.Red;
+                label2.Visible = true;
+                label2.Visible = true;
+
+
+
+            }
+            catch (Exception ex1)
+            {
+                Console.WriteLine("Exception :" + ex1.ToString());
+                label2.Text = "Error saving details succefully to system";
+                label2.ForeColor = Color.Red;
+                label2.Visible = true;
+                label2.Visible = true;
+            }
+
+        }
+    }
+}
